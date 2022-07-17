c2d02aae2636cd0396d3663ea5759b5feb84050a
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-07-16 23:17:14 -0400

    Update resolvers.js
    Added the resolvers to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,7 @@ const { AuthenticationError } = require('apollo-server-express');
 // token variable to create users
 const { signToken } = require('../utils/auth');
 
-const Resolvers = {
+const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             // check if there is a user in the context
@@ -26,6 +26,49 @@ const Resolvers = {
             
             return { user, token};
         },
-        login: asunc
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            // if there's no user then it should throw an error
+            if(!user) {
+                throw new AuthenticationError('Invalid login information');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw) {
+                throw new AuthenticationError('Invalid login information');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (_parent, { input }, context) => {
+            if (context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: input } },
+                    { new: true }
+                );
+
+                return updateUser;
+            }
+            throw new AuthenticationError('You must be logged in to access this data');
+        },
+        deleteBook: async (_parent, {bookId}, context) => {
+            console.log('resolver: ', bookId);
+            if (context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { _id: bookId } } },
+                    { new: true }
+                );
+
+                return updateUser;
+            }
+            throw new AuthenticationError('You must be logged in to access this data');
+        }
     }
 }
+
+module.exports = resolvers;
